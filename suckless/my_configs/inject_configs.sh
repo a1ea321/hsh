4c1f9165ad7d1f98afae62bf84a48ea019c39441
alp <alp@ax>
2021-01-25 09:12:24 +0100

    reorganize suckless/

--- suckless/my_configs/inject_configs.sh
+++ suckless/my_configs/inject_configs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-
 # After cloning a suckless repo, I need to edit the configuration files.
 # This script (over)writes my configuration files into respective directories.
 
-# Assumed directory structure is below.
-# Symbolic links to the files marked by ** (my files) will be created in the)
-# directories marked by * (directories produced by `git clone`)
+# Assumed directory structure is below for a suckless/ containing just slock and
+# st. Symbolic links to the files marked by ** (my files) will be created in the
+# directories marked by * (directories produced by `git clone`).
+
+# TODO (maybe): Auto apply patches (if they become too many)
 
 # ~/hsh_folder_whatever_the_name/suckless/
-# ├── dmenu
 # ├── my_configs
 # │   ├── inject_configs.sh <- This script
 # │   ├── slock
@@ -18,24 +18,36 @@
 # │   └── st
 # │       ├── config.h **
 # │       └── config.mk **
-# ├── slock *
-# │   ├── LICENSE
-# │   │...
-# │   └── util.h
-# └── st *
-#     ├── FAQ
-#     │...
-#     └── x.c
+# ├── patches
+# │   ├── slock *
+# │   │   ├── some_arbitrarily_named_patch.diff
+# │   │   │...
+# │   │   └── some_other_patch.diff
+# │   └── st *
+# │       └── st_patch.diff
+# └── repos # projects cloned (as git submodules) from suckless's git repos
+#     ├── slock *
+#     │   ├── LICENSE
+#     │   │...
+#     │   └── util.h
+#     └── st *
+#         ├── FAQ
+#         │...
+#         └── x.c
 
-
+# Make sure we are in my_configs/, whereever it might be.
 DIR="$(
     cd "$(dirname ${BASH_SOURCE[0]})"
     pwd
 )"
 cd $DIR
 
-for repo in $(ls -d */ | cut -f1 -d/); do
-    for config_file in $(find $repo -type f); do
-        ln --symbolic --force $DIR/$config_file ../$config_file
+for repo in $(ls ../repos); do
+    pushd ../repos/$repo
+    for config_file in $(ls ../../my_configs/$repo); do
+        echo -n "Symlinking $repo/$config_file... "
+        ln --symbolic --force ../../my_configs/$repo/$config_file &&
+        echo DONE || echo FAILED
     done
+    popd
 done
