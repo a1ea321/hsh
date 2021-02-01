4b08e0f57b28e72ce7386502867f68f04afc1272
alp <alp@ax>
2021-02-01 19:44:13 +0100

    not important
    add some screen layouts (thanks ARandR)

---
+++ link_at_home/.screenlayout/VGA1_1680x1050__LVDS1_1600x900.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output LVDS1 --primary --mode 1600x900 --pos 1680x0 --rotate normal --output DP1 --off --output HDMI1 --off --output VGA1 --mode 1680x1050 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output VGA-1-2 --off
