f6e10f662ca81c0641279088f06b62356f471cad
alp <alp@ax>
2021-02-04 10:26:21 +0100

    add script to make suckless repos to init/submodules.sh

--- init/submodules.sh
+++ init/submodules.sh
@@ -4,5 +4,21 @@
 
 # Script may not work. Should hint nevertheless.
 
+cd ~/.hsh || {
+    echo 'Did you run ~/.hsh/init/init_hsh.sh?'
+    exit 1
+}
+
 git submodule update --init --recursive
 ~/.hsh/suckless/my_configs/inject_configs.sh
+
+command -v 
+
+cd ~/.hsh/suckless/repos
+for repo in `ls`; do
+    cd $repo
+    make &&
+    read -p "`make $repo` successful. You may 'make install' it as root." ||
+    read -p "Problem making $repo. Press ENTER to continue."
+    cd ..
+done
